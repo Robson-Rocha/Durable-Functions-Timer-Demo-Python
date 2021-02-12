77c375c0b30bb0ae2b4de9576b8504cf38ca1572
Robson Rocha de Araújo <rrocha.araujo@gmail.com>
2021-02-11 23:05:20 -0300

    Update __init__.py

--- finish_process/__init__.py
+++ finish_process/__init__.py
@@ -6,8 +6,6 @@ from shared_code.key_value_store import KeyValueStore
 
 
 async def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-
     instanceid = req.params.get('instanceid')
     result = req.params.get('result')
 
